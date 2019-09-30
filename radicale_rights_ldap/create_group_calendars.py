37e0e6c3a106312f1e1f320b2299cb32a3ca42bb
Finn Krein <finnkrein@googlemail.com>
2019-09-30 18:10:36 +0200

    Fix several bugs/typos

--- radicale_rights_ldap/create_group_calendars.py
+++ radicale_rights_ldap/create_group_calendars.py
@@ -2,9 +2,11 @@
 
 from os import path, scandir, symlink
 import radicale
+import sys
 
 
-def create_collection(user,
+def create_collection(collection_class,
+                      user,
                       displayname,
                       calendar_description,
                       color="#a1bc9bff"):
@@ -17,7 +19,7 @@ def create_collection(user,
         "is_group_calendar": True,
     }
     uuid = radicale.storage.random_uuid4()
-    Collection.create_collection(path.join(user, uuid), props=props)
+    collection_class.create_collection(path.join(user, uuid), props=props)
 
 
 def visible_subdirs(path):
@@ -28,14 +30,15 @@ def visible_subdirs(path):
 
 # Create collection for every group in calendar_groups,
 # if it doesn't already exists
-def create_group_calendar(group):
-    collections = list(Collection.discover(group, depth=1))
+def create_group_calendar(group, collection_class):
+    collections = list(collection_class.discover(group, depth=1))
     group_calendar_exists = False
     for collection in collections:
         if collection.get_meta().get("is_group_calendar", False):
             group_calendar_exists = True
     if not group_calendar_exists:
         create_collection(
+            collection_class,
             group,
             "{} group calendar".format(group),
             "Default group calendar of {}".format(group),
@@ -43,7 +46,7 @@ def create_group_calendar(group):
 
 
 def main():
-    config = radicale.config.load([sys.argv[0]])
+    config = radicale.config.load([sys.argv[1]])
     storepath = path.join(config.get("storage", "filesystem_folder"),
                           "collection-root")
     radicale_users = visible_subdirs(storepath)
@@ -53,9 +56,9 @@ def main():
     calendar_groups = list(map(str.strip,
                                rights_conf["managed_groups"].split(",")))
     for group in calendar_groups:
-        create_group_calendar(group)
+        create_group_calendar(group, Collection)
     return 0
 
 
-if name = "__main__":
+if __name__ == "__main__":
     main()
