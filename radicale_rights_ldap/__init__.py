7ab0056500d5a0b778091e529bdac6bada03feb4
Finn Krein <finnkrein@googlemail.com>
2019-12-06 17:48:38 +0100

    Add group_prefix
    Our ldap system recently got the requierement that every
    group in the department has to be prefixed. To keep the group names
    the same and save everybody the pain to enter the prefix everytime
    I added this option.

--- radicale_rights_ldap/__init__.py
+++ radicale_rights_ldap/__init__.py
@@ -11,16 +11,15 @@ class Rights(rights.BaseRights):
         self.server = Server(self.configuration.get("rights", "ldap_url"))
         self.base = self.configuration.get("rights", "user_base")
         self.group_base = self.configuration.get("rights", "group_base")
-        self.user_attribute = self.configuration.get("rights",
-                                                     "user_attribute")
-        self.group_attribute = self.configuration.get("rights",
-                                                      "group_attribute")
-        self.filter = self.configuration.get("rights",
-                                             "ldap_filter",
-                                             fallback="(objectClass=*)")
-        self.scope = self.configuration.get("rights",
-                                            "ldap_scope",
-                                            fallback="SUBTREE")
+        self.user_attribute = self.configuration.get("rights", "user_attribute")
+        self.group_attribute = self.configuration.get("rights", "group_attribute")
+        self.group_prefix = self.configuration.get(
+            "rights", "group_prefix", fallback=None
+        )
+        self.filter = self.configuration.get(
+            "rights", "ldap_filter", fallback="(objectClass=*)"
+        )
+        self.scope = self.configuration.get("rights", "ldap_scope", fallback="SUBTREE")
         binddn = self.configuration.get("rights", "ldap_binddn")
         password = self.configuration.get("rights", "ldap_password")
         self.connection = Connection(self.server, binddn, password)
@@ -31,15 +30,18 @@ class Rights(rights.BaseRights):
             self.connection.rebind()
         except Exception as e:
             raise RuntimeError(
-                "LDAP rights plugn failed to connect to LDAP server: {}".
-                format(e.args))
+                "LDAP rights plugn failed to connect to LDAP server: {}".format(e.args)
+            )
 
     def user_in_group(self, user, group):
         if not self.connection.bound:
             self.bind()
         user_filter = self.user_attribute + "=" + user
-        group_filter = "(memberOf={}={},{})".format(self.group_attribute,
-                                                    group, self.group_base)
+        if self.group_prefix is not None:
+            group = self.group_prefix + group
+        group_filter = "(memberOf={}={},{})".format(
+            self.group_attribute, group, self.group_base
+        )
         query = "(& ({}) {} {})".format(user_filter, group_filter, self.filter)
         if self.connection.search(self.base, query, search_scope=self.scope):
             return True
@@ -48,8 +50,11 @@ class Rights(rights.BaseRights):
 
     def authorized(self, user, path, permissions):
         self.logger.debug(
-            "User %r is trying to access path %r. Permissions: %r", user, path,
-            permissions)
+            "User %r is trying to access path %r. Permissions: %r",
+            user,
+            path,
+            permissions,
+        )
         # everybody can access the root collection
         if path == "/":
             self.logger.debug("Accessing root path. Access granted.")
@@ -66,10 +71,10 @@ class Rights(rights.BaseRights):
             in_group = self.user_in_group(user, pathowner)
             if in_group:
                 self.logger.debug(
-                    "User %r is in pathowner group %r. Access granted.", user,
-                    pathowner)
+                    "User %r is in pathowner group %r. Access granted.", user, pathowner
+                )
             else:
                 self.logger.debug(
-                    "Access to path %r is not granted to user %r.", pathowner,
-                    user)
+                    "Access to path %r is not granted to user %r.", pathowner, user
+                )
             return in_group
